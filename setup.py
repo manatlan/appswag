e3bf20a066e9ff45bcaa23d71afea91c35a365c7
manatlan <manatlan@gmail.com>
2021-03-16 09:11:11 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 version = open('pyswagger/__init__.py', 'r').readline().split()[2].strip("'")
 
 setup(
-    name = 'pyswagger',
+    name = 'appswag',
     packages = find_packages(exclude=['*.tests.*']),
     version = version,
     description = 'A type-safe, dynamic, spec-compliant swagger client & converter for python',
-    author = 'Mission Liao',
+    author = 'Mission Liao / manatlan',
     author_email = 'missionaryliao@gmail.com',
-    url = 'https://github.com/mission-liao/pyswagger', # use the URL to the github repo
-    download_url = 'https://github.com/mission-liao/pyswagger/tarball/{0}'.format(version),
+    url = 'https://github.com/manatlan/appswag', # use the URL to the github repo
+    download_url = 'https://github.com/manatlan/appswag/tarball/{0}'.format(version),
     keywords = ['swagger', 'REST'], # arbitrary keywords
     classifiers = [
         'Development Status :: 4 - Beta',
